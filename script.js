5e760be3d707e069bd3529b0f4db2baaa62a1822
cuongBug <vumcuong2003@gmail.com>
2024-12-03 08:37:51 +0700

    b8-js4

---
+++ script.js
@@ -0,0 +1,81 @@
+// console.log(location.href);
+
+// var currentURL = window.location.href;
+// console.log(currentURL);
+
+// console.log(location.protocol);
+// console.log(location.hostname);
+// console.log(location.port);
+
+// var protocol = window.location.protocol;
+// var hostname = window.location.hostname;
+// var port = window.location.port;
+
+// console.log("Protocol: " + protocol);
+// console.log("Hostname: " + hostname);
+// console.log("Port: " + port);
+
+// const open = (ursl, delay) => {
+   
+//     setTimeout(() =>{
+//         window.location.href = ursl;
+//     }, delay);
+// };
+
+// open("https://www.google.com/search?q=google+map&rlz=1C1ONGR_enVN1085VN1085&oq=g&gs_lcrp=EgZjaHJvbWUqDggBEEUYJxg7GIAEGIoFMgYIABBFGDwyDggBEEUYJxg7GIAEGIoFMg4IAhBFGCcYOxiABBiKBTIGCAMQRRg8MgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQgxMjAwajBqN6gCALACAA&sourceid=chrome&ie=UTF-8", 3000);
+
+// const relod = () => {
+//     setTimeout(() => {
+//         location.reload();
+//     }, 3000)
+// };
+
+// relod();
+
+// var issuer = window.location.protocol === "https:";
+// console.log("an toan: " + issuer);
+
+// window.print();
+// let time = 10;
+// const countdow = () => {
+
+//         if(time >= 0){
+//             console.log(time);
+//             time--;            
+//             setTimeout(countdow, 1000);//hien thi dem nguoc
+//         }else{
+//             console.log("happy new year");
+//         }
+// }
+// countdow();
+
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+
+// console.log(window.screenX);
+// console.log(window.screenY);
+
+// window.open("https://www.google.com/search?q=google+map&rlz=1C1ONGR_enVN1085VN1085&oq=g&gs_lcrp=EgZjaHJvbWUqDggBEEUYJxg7GIAEGIoFMgYIABBFGDwyDggBEEUYJxg7GIAEGIoFMg4IAhBFGCcYOxiABBiKBTIGCAMQRRg8MgYIBBBFGDwyBggFEEUYPDIGCAYQRRg8MgYIBxBFGDzSAQgxMjAwajBqN6gCALACAA&sourceid=chrome&ie=UTF-8", "cuong", "width: 500, height: 600, left: 0, right: 0");
+
+
+// window.open(
+//     "<https://daca.vn>",
+//     "myWindow",
+//     "width=1200,height=600,top=100,left=100"
+//   );
+
+// console.log(navigator.userAgent);
+// console.log(navigator.language);
+
+// console.log(window.screen.width);
+// console.log(window.screen.height);
+
+// const isMobile = navigator.userAgent.mobible;
+
+// if(isMobile){
+//     console.log("mobile");
+// }else{
+//     console.log("pc");
+// }
+
+// console.log(navigator.languages);
